14719dc95ceaf874f6084596c119cc6710792450
Jeff Jerousek <37905627+JeffJerousek@users.noreply.github.com>
2018-10-03 15:43:00 -0500

    Add files via upload

---
+++ Install-WindowsFeatures.ps1
@@ -0,0 +1,23 @@
+﻿Function Install-WindowsFeatures
+{
+[CmdletBinding()]
+param (
+        [string]$Filter = '',
+        [validateSet('Installed',"NotPresent")]
+        [string]$State
+        )
+
+If ($state)
+{
+Get-WindowsCapability -Online -name ($filter + '*') |
+ Select-Object DisplayName, State, Name | where {$_.state -eq $state} |
+  Out-GridView -Title "Select Features on Demand" -OutputMode Multiple | ForEach-Object {Add-WindowsCapability -Online -Name $_.name} 
+  }
+else
+{
+Get-WindowsCapability -Online -name ($filter + '*') |
+ Select-Object DisplayName, State, Name |
+  Out-GridView -Title "Select Features on Demand" -OutputMode Multiple | ForEach-Object {Add-WindowsCapability -Online -Name $_.name} 
+}
+
+}
